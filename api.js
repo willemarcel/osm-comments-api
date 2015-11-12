ecbc023b536952eebb8219b78b2e273ecf22895c
Sanjay Bhangar <sanjay@mapbox.com>
2015-11-12 19:57:43 +0530

    add total results to response, remove 'opened_by' for now, some cleanups

--- api.js
+++ api.js
@@ -1,34 +1,41 @@
 var express = require('express');
 var pg = require('pg');
 var queue = require('queue-async');
-var notesQuery = require('./query/notes');
+var notesQueries = require('./query/notes');
 var Note = require('./lib/Note');
 var pgURL = require('./lib/config')().PostgresURL;
 
 var server = module.exports = express();
 
 server.get('/api/v1/notes', function(req, res) {
-    var notesQ = notesQuery(req.query);
-    console.log('notesQ', notesQ);
-    var sql = notesQ.text;
-    var params = notesQ.values;
+    var searchQuery = notesQueries.getSearchQuery(req.query);
+    // console.log('notesQ', notesQ);
+    // var searchSQL = searchSQL.text;
+    // var searchParams = searchSQL.values;
+    var countQuery = notesQueries.getCountQuery(req.query);
+    console.log('queries', searchQuery, countQuery);
     var q = queue(5);
     pg.connect(pgURL, function(err, client, done) {
-        q.defer(client.query.bind(client), sql, params);
+        q.defer(client.query.bind(client), searchQuery.text, searchQuery.values);
+        q.defer(client.query.bind(client), countQuery.text, countQuery.values);
         q.awaitAll(function(err, results) {
             if (err) {
                 console.log('query error', err);
             }
-            var notesResult = results[0];
-            var notesGeoJSON = notesResult.rows.map(function(row) {
-                console.log('row', row);
+
+            var searchResult = results[0];
+            var countResult = results[1];
+            var notesArray = searchResult.rows.map(function(row) {
+                // console.log('row', row);
                 var note = new Note(row);
-                console.log('note', note);
+                // console.log('note', note);
                 return note.getGeoJSON();
             });
+            var count = countResult.rows[0].count;
             var featureCollection = {
                 'type': 'FeatureCollection',
-                'features': notesGeoJSON
+                'features': notesArray,
+                'total': count
             };
             res.json(featureCollection);
         });
--- query/notes.js
+++ query/notes.js
@@ -1,24 +1,37 @@
 var squel = require('squel').useFlavour('postgres');
 var moment = require('moment');
 
-module.exports = function(params) {
+module.exports = {};
+
+module.exports.getSearchQuery = getSearchQuery;
+module.exports.getCountQuery = getCountQuery;
+
+function getSearchQuery(params) {
+    var sql = squel.select()
+        .from('notes')
+        .field('notes.id', 'note_id')
+        .field('notes.created_at', 'created_at')
+        .field('notes.closed_at', 'closed_at')
+        .field('ST_AsGeoJSON(notes.point)', 'point');
+ 
+    sql = addWhereClauses(sql, params);
+    sql = addOffsetLimit(sql, params);
+    return sql.toParam();
+}
+
+function getCountQuery(params) {
+    var sql = squel.select()
+        .from('notes')
+        .field('count(notes.id)');
+    sql = addWhereClauses(sql, params);
+    return sql.toParam();
+}
+
+function addWhereClauses(sql, params) {
     var from = params.from || null;
     var to = params.to || null;
     var users = params.users || null;
     var bbox = params.bbox || null;
-    var offset = params.offset || 0;
-    var limit = params.limit || 20;
-    var sql = squel.select()
-        .from('notes')
-        .left_outer_join('users', 'u1', 'notes.opened_by = u1.id')
-        .field('notes.id', 'note_id')
-        .field('notes.created_at', 'created_at')
-        .field('notes.closed_at', 'closed_at')
-        .field('ST_AsGeoJSON(notes.point)', 'point')
-        .field('u1.id', 'user_id')
-        .field('u1.name', 'user_name')
-        .offset(Number(offset))
-        .limit(Number(limit));
     if (from) {
         sql.where('created_at > ?', from);
     }
@@ -33,6 +46,14 @@ module.exports = function(params) {
         });
         sql.where('users.name in ?', usersArray);
     }
-    return sql.toParam();
-};
+    sql.distinct();
+    return sql;
+}
 
+function addOffsetLimit(sql, params) {
+    var offset = params.offset || 0;
+    var limit = params.limit || 20;
+    sql.offset(Number(offset))
+        .limit(Number(limit));
+    return sql;
+}
