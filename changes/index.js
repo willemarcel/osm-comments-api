25e4fa37265a8893211e985f8daf50c8885a3846
Sajjad Anwar <me@sajjad.in>
2017-02-09 16:09:16 +0530

    tag queries

--- changes/index.js
+++ changes/index.js
@@ -64,55 +64,91 @@ function validateParams(params) {
 }
 
 function getQuery(from, to, users, tags, callback) {
-    console.log('preparing query for', users);
 
-    var sql = squel.select()
+    var sql = squel.select({'parameterCharacter': '!!'})
         .from('stats')
-        .where('change_at > ?', from)
-        .where('change_at < ?', to);
+        .where('change_at > !!', from)
+        .where('change_at < !!', to);
+
+    if (tags) {
+        var tagsArray = tags.split(',').map(function(tag) {
+            return tag;
+        });
+        prepareTagQuery(tagsArray, sql, function(err, sql) {
+            if (err) {
+                return callback(err);
+            }
+        });
+    }
 
     if (users) {
         var usersArray = users.split(',').map(function(user) {
             return user;
         });
 
-        console.log(usersArray);
-
-        var userSql = squel.select()
-            .from('users')
-            .field('id')
-            .where('name in ?', usersArray);
-
-        var userIds = [];
-        pg.connect(pgURL, function(err, client, done) {
+        getUserIds(usersArray, function(err, userIds) {
             if (err) {
-                return callback(err, null);
+                return callback(err);
             }
-            client.query(userSql.toParam(), function(err, result) {
-                done();
-                if (err) {
-                    return callback(err);
-                }
 
-                if (result.rows.length === 0) {
-                    return callback(new errors.NotFoundError('No such users'));
-                }
-                result.rows.forEach(function (r) {
-                    userIds.push(r.id);
-                });
-                sql.where('uid in ?', userIds);
-
-                if (tags) {
-                    var tagsArray = tags.split(',').map(function(tag) {
-                        return tag;
-                    });
-                    console.log('# tags', tagsArray);
-
-                }
-                callback(null, sql.toParam());
-            });
+            sql.where('uid in !!', userIds);
+            callback(null, sql.toParam());
         });
     } else {
+
         callback(null, sql.toParam());
     }
+
+
+}
+
+function getUserIds(users, callback) {
+    var userSql = squel.select({'parameterCharacter': '!!'})
+        .from('users')
+        .field('id')
+        .where('name in !!', users);
+
+    var userIds = [];
+    pg.connect(pgURL, function(err, client, done) {
+        if (err) {
+            return callback(err, null);
+        }
+        client.query(userSql.toParam(), function(err, result) {
+            done();
+            if (err) {
+                return callback(err);
+            }
+
+            if (result.rows.length === 0) {
+                return callback(new errors.NotFoundError('No such users'));
+            }
+            result.rows.forEach(function (r) {
+                userIds.push(r.id);
+            });
+            callback(null, userIds);
+
+        });
+    });
+}
+
+function prepareTagQuery(tags, sql, callback) {
+    // SELECT * FROM json_test WHERE data ? 'a'; - check if 'a' exists as a key.
+    // SELECT * FROM json_test WHERE data ?| array['a', 'b'];
+    // select tags_created from stats where tags_created -> 'shop' ? 'tattoo';
+
+    var tagsSql = squel.expr();
+    tagsSql.options.parameterCharacter = '!!';
+
+    tags.forEach(function (tag) {
+        var key = tag.split('=')[0];
+        var value = tag.split('=')[1];
+        if (value !== '*') {
+            tagsSql.or('tags_created -> !! ? !!', key, value);
+            tagsSql.or('tags_modified -> !! ? !!', key, value);
+        } else {
+            tagsSql.or('tags_created ? !!', key);
+            tagsSql.or('tags_modified ? !!', key);
+        }
+    });
+    callback(null, sql.where(tagsSql));
 }
