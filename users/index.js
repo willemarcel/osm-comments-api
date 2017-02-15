8c7a2956d07764899c16891cc254ac457800e8b1
Sanjay Bhangar <sanjay@mapbox.com>
2017-02-15 16:28:59 +0530

    accepts an extra query param to fetch extra attributes for a user, refs #59

--- users/index.js
+++ users/index.js
@@ -10,15 +10,25 @@ var pgURL = config.PostgresURL;
 var pgPromise = helpers.pgPromise;
 var promisifyQuery = helpers.promisifyQuery;
 
-users.getName = function(name) {
-    return query(name, 'name');
+users.getName = function(name, queryParams, callback) {
+    var extra = false;
+	if (queryParams.extra) extra = true;
+    return query(name, 'name', extra);
+}
+
+users.getId = function(id, query, callback) {
+    var extra = false;
+    if (query.extra) extra = true;
+    query(id, 'id', extra);
 };
 
-users.getId = function(id) {
-    return query(id, 'id');
-};
 
-function query(value, thing) {
+/*
+    @param {String} value - should be either the user id or username being searched for
+    @param {String} thing - either `id` or `name`, corresponding to type of value provided
+    @param {Boolean} extra - whether to fetch extra attributes for the user
+*/
+function query(value, thing, extra) {
     var userQuery = 'SELECT * FROM users WHERE ' + thing + '= $1';
     return pgPromise(pgURL)
         .then(function (pg) {
@@ -37,6 +47,36 @@ function query(value, thing) {
             if (result.rows.length === 0) {
                 return Promise.reject(new errors.NotFoundError('User not found'));
             }
-            return result.rows[0];
+            var user = result.rows[0];
+			if (extra) {
+				 return fetchExtra(user, client);
+			}
         });
+    });
+}
+
+/*
+    @param {Object} user - basic user details
+    @param {PGClient Object} client - a postgres client connection instance
+    @param {Function} done - function to call to close connection to db
+    @param {Function} callback - function to call with basic data augmented with extra attributes
+*/
+function fetchExtra(user, client, done, callback) {
+    var userId = user.id;
+    var q = queue(3);
+    var totalDiscussionsQ = 'SELECT COUNT(id) FROM changeset_comments WHERE user_id=$1';
+    var discussedChangesetsQ = 'SELECT COUNT(id) FROM changesets WHERE (SELECT COUNT(id) FROM changeset_comments WHERE changeset_comments.changeset_id = changesets.id) > 0 AND changesets.discussion_count > 0 AND changesets.user_id = $1';
+    q.defer(client.query.bind(client), totalDiscussionsQ, [userId]);
+    q.defer(client.query.bind(client), discussedChangesetsQ, [userId]);
+    q.awaitAll(function(err, results) {
+        done();
+        if (err) {
+            return callback(err);
+        }
+        user.extra = {
+            'total_discussions': results[0].rows[0].count,
+            'changesets_with_discussions': results[1].rows[0].count
+        };
+        return callback(null, user);
+    });
 }
