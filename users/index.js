02f4f9e10aab4ce26b85c6f5e9ede24d86bc0553
Kushan Joshi <0o3ko0@gmail.com>
2017-03-27 17:31:38 +0530

    Fortify with promises

--- users/index.js
+++ users/index.js
@@ -10,16 +10,16 @@ var pgURL = config.PostgresURL;
 var pgPromise = helpers.pgPromise;
 var promisifyQuery = helpers.promisifyQuery;
 
-users.getName = function(name, queryParams, callback) {
+users.getName = function(name, queryParams) {
     var extra = false;
-	if (queryParams.extra) extra = true;
+    if (queryParams.extra) extra = true;
     return query(name, 'name', extra);
-}
+};
 
-users.getId = function(id, query, callback) {
+users.getId = function(id, query) {
     var extra = false;
     if (query.extra) extra = true;
-    query(id, 'id', extra);
+    return query(id, 'id', extra);
 };
 
 
@@ -33,53 +33,54 @@ function query(value, thing, extra) {
     return pgPromise(pgURL)
         .then(function (pg) {
             var query = promisifyQuery(pg.client);
-            return query(userQuery, [value])
-            .then(function (r) {
+            var userQueryProm = query(userQuery, [value]);
+
+            return userQueryProm.then(function (result) {
+                if (result.rows.length === 0) {
+                    return Promise.reject(new errors.NotFoundError('User not found'));
+                }
+                var user = result.rows[0];
+
+                if (extra) {
+                    return fetchExtra(user, query);
+                }
+                return user;
+            })
+            .then(function (result) {
                 pg.done();
-                return r;
+                return result;
             })
             .catch(function (e) {
                 pg.done();
                 return Promise.reject(e);
             });
-        })
-        .then(function (result) {
-            if (result.rows.length === 0) {
-                return Promise.reject(new errors.NotFoundError('User not found'));
-            }
-            var user = result.rows[0];
-			if (extra) {
-				 return fetchExtra(user, client);
-			}
         });
-    });
 }
 
 /*
     @param {Object} user - basic user details
-    @param {PGClient Object} client - a postgres client connection instance
-    @param {Function} done - function to call to close connection to db
-    @param {Function} callback - function to call with basic data augmented with extra attributes
+    @param {query}  promisified query which is identical to client.query
+                    except that it is thenable
 */
-function fetchExtra(user, client, done, callback) {
+function fetchExtra(user, query) {
     var userId = user.id;
-    var q = queue(3);
     var totalDiscussionsQ = 'SELECT COUNT(id) FROM changeset_comments WHERE user_id=$1';
     var discussedChangesetsQ = 'SELECT COUNT(id) FROM changesets WHERE (SELECT COUNT(id) FROM changeset_comments WHERE changeset_comments.changeset_id = changesets.id) > 0 AND changesets.discussion_count > 0 AND changesets.user_id = $1';
-    var mappingDaysQ = "SELECT COUNT(DISTINCT(date_trunc('day', created_at))) FROM changesets WHERE user_id=$1";
-    q.defer(client.query.bind(client), totalDiscussionsQ, [userId]);
-    q.defer(client.query.bind(client), discussedChangesetsQ, [userId]);
-    q.defer(client.query.bind(client), mappingDaysQ, [userId]);
-    q.awaitAll(function(err, results) {
-        done();
-        if (err) {
-            return callback(err);
-        }
-        user.extra = {
-            'total_discussions': results[0].rows[0].count,
-            'changesets_with_discussions': results[1].rows[0].count,
-            'mapping_days': results[2].rows[0].count
-        };
-        return callback(null, user);
-    });
+    var mappingDaysQ = 'SELECT COUNT(DISTINCT(date_trunc(\'day\', created_at))) FROM changesets WHERE user_id=$1';
+
+    var promises = [
+        query(totalDiscussionsQ, [userId]),
+        query(discussedChangesetsQ, [userId]),
+        query(mappingDaysQ, [userId])
+    ];
+  
+    return Promise.all(promises)
+        .then(function (results) {
+            user.extra = {
+                'total_discussions': results[0].rows[0].count,
+                'changesets_with_discussions': results[1].rows[0].count,
+                'mapping_days': results[2].rows[0].count
+            };
+            return user;
+        });
 }
